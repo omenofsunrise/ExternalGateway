d113d20e3a9ada45c37872a5debe401aa1b8e7e0
omenofsunrise <ch3rnow.c@yandex.ru>
2026-07-15 00:03:19 +0300

    add common add header function

--- internal/adapter/common/client.go
+++ internal/adapter/common/client.go
@@ -1,6 +1,9 @@
 package common
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+)
 
 type BaseClient struct {
 	HttpClient *http.Client
@@ -20,4 +23,13 @@ func NewBaseClient(httpClient *http.Client, apiKey, baseUrl string) *BaseClient 
 func NewBaseAIClient(httpClient *http.Client, apiKey, baseUrl, model string) *BaseAIClient {
 	baseClient := NewBaseClient(httpClient, apiKey, baseUrl)
 	return &BaseAIClient{BaseClient: *baseClient, Model: model}
+}
+
+func AddHeader(req *http.Request) {
+	req.Header.Add("Content-Type", "application/json")
+}
+
+func AddAuthHeader(req *http.Request, apiKey string) {
+	AddHeader(req)
+	req.Header.Add("Authorization", fmt.Sprintf("Bearer %s", apiKey))
 }
